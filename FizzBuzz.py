88dd5a332c149be58f5478a848f601c167f9ea78
kirillkelin <kelinkirill@gmail.com>
2023-09-10 16:52:18 +0300

    FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+N = int(input())
+list = []
+for i in range(1, N+1):
+    if i % 15 == 0:
+        list.append('Fizz Buzz')
+        continue
+    elif i % 3 == 0:
+        list.append('Fizz')
+    elif i % 5 == 0:
+        list.append('Buzz')
+    else:
+        list.append(i)
+print(*list, sep=', ')
