d16083d42070e5460bf177a8463f835780f23b34
kirillkelin <kelinkirill@gmail.com>
2023-09-10 18:05:30 +0300

    Создание двумерного списка

--- Two_Dimensional_List.py
+++ Two_Dimensional_List.py
@@ -1,1 +1,7 @@
-print("Hello")
+N, M = list(map(int, input().split()))
+arr = [[0] * M for i in range(N)]
+for i in range(N):
+    for j in range(M):
+        arr[i][j] = (i + 1) * (j + 1)
+        print(arr[i][j], end=' ')
+    print()
