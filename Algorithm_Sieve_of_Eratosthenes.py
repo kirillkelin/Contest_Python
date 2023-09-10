567e2b5e0c3fb744aed3a51d8db29a503e2da4f8
kirillkelin <kelinkirill@gmail.com>
2023-09-10 16:46:34 +0300

    Решето Эратосфена

---
+++ Algorithm_Sieve_of_Eratosthenes.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+primes = [i for i in range(30*N + 1)]
+primes[1] = 0
+
+i = 2
+while i <= 30*N:
+    if primes[i] != 0:
+        j = i + i
+        while j <= 30*N:
+            primes[j] = 0
+            j = j + i
+    i += 1
+
+primes = [i for i in primes if i != 0]
+print(primes[N - 1])
